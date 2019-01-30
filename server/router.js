6414e38148f596001d3f1f03e512976260af63de
Sasha <sashguiab@gmail.com>
2019-01-29 16:18:56 -0800

    added get route to mongo instance

--- server/router.js
+++ server/router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
-
+const controller = require('./controller.js')
 // define the routes to each controller method below
 
+router.route('/')
+  .get(controller.get);
 
 
 module.exports = router;
